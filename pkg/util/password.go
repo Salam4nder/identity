462364109ecd5aafff3a1f321e57027ba65378f0
Salam4nder <kamran.personal@icloud.com>
2023-03-06 20:08:46 +0100

    add password.go, helper functions for hashing and validating passwords

---
+++ pkg/util/password.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword hashes a password using bcrypt.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+// ValidatePassword validates a password using bcrypt.
+func ValidatePassword(password, hash string) error {
+	return bcrypt.CompareHashAndPassword(
+		[]byte(hash), []byte(password))
+}
