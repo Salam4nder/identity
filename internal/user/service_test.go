5947e317cd5c4ae8674e53fca174c4db3ee579c4
Salam4nder <kamran.personal@icloud.com>
2023-03-06 21:04:45 +0100

    add FindOneByID tests

--- internal/user/service_test.go
+++ internal/user/service_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Salam4nder/user/pkg/util"
 	"github.com/stretchr/testify/assert"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
 
@@ -55,5 +57,44 @@ func TestInsertOne(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.Empty(t, id)
 		assert.IsType(t, id, "")
+	})
+}
+
+func TestFindOneByID(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("success returns user and nil", func(mt *mtest.T) {
+		idObj := primitive.NewObjectID()
+		mt.AddMockResponses(
+			mtest.CreateCursorResponse(
+				1, "foo.bar", mtest.FirstBatch, bson.D{
+					{Key: "_id", Value: idObj},
+					{Key: "full_name", Value: "John Doe"},
+					{Key: "email", Value: "lmao@gmail.com"},
+				}))
+
+		service := NewService(mt.Coll)
+
+		user, err := service.FindOneByID(context.TODO(), idObj.Hex())
+
+		assert.Nil(t, err)
+		assert.NotNil(t, user)
+		assert.IsType(t, user, User{})
+		assert.Equal(t, user.ID, idObj)
+		assert.Equal(t, user.FullName, "John Doe")
+		assert.Equal(t, user.Email, "lmao@gmail.com")
+	})
+
+	mt.Run("error returns empty user and err", func(mt *mtest.T) {
+		mt.AddMockResponses(mtest.CreateWriteErrorsResponse())
+
+		service := NewService(mt.Coll)
+
+		user, err := service.FindOneByID(context.TODO(), "123")
+
+		assert.NotNil(t, err)
+		assert.Empty(t, user)
+		assert.IsType(t, user, User{})
 	})
 }
