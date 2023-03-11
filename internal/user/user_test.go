b0739cad8aa87fd130107e68476940d768d341f5
Salam4nder <kamran.personal@icloud.com>
2023-03-11 17:31:42 +0100

    update Test name to match new function name

--- internal/user/user_test.go
+++ internal/user/user_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func ValidateCreateParam(t *testing.T) {
+func ValidateInsertOneParam(t *testing.T) {
 	tests := []struct {
 		name    string
 		param   InsertOneParam
