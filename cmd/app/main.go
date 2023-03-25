b32ea03148495000c24880dcb2fc8843fea43625
Salam4nder <kamran.personal@icloud.com>
2023-03-25 18:43:05 +0100

    Add entry point

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"context"
+	"time"
+
+	"github.com/Salam4nder/user/internal/config"
+	"github.com/Salam4nder/user/internal/grpc"
+
+	"github.com/Salam4nder/user/internal/storage"
+	"github.com/Salam4nder/user/pkg/mongo"
+
+	"github.com/Salam4nder/inventory/pkg/logger"
+)
+
+const bootTimeout = 20 * time.Second
+
 func main() {
-	println("wip")
+	cfg, err := config.New()
+	panicOnErr(err)
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		bootTimeout,
+	)
+	defer cancel()
+
+	mongoDB, err := mongo.New(ctx, cfg.Mongo)
+	panicOnErr(err)
+
+	userStorage := storage.NewUserStorage(
+		mongoDB.GetCollection())
+
+	logger, err := logger.New("")
+
+	service := grpc.NewUserService(userStorage)
+
+	server := grpc.NewServer(service, &cfg.Server, logger)
+
+	err = server.Serve()
+	panicOnErr(err)
+}
+
+func panicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
