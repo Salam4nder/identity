6b91023cedc3509dc649ec66fa194da4d02f9eff
Salam4nder <kamran.personal@icloud.com>
2023-03-26 18:30:51 +0200

    Delete uncommon errors

--- internal/grpc/error.go
+++ internal/grpc/error.go
@@ -9,10 +9,6 @@ func requestIsNilError() error {
 	return status.Error(codes.InvalidArgument, "request is nil")
 }
 
-func invalidIDError() error {
-	return status.Error(codes.InvalidArgument, "invalid id")
-}
-
 func internalServerError() error {
 	return status.Error(codes.Internal, "internal server error")
 }
