5f7d00ac2e480a5c9eb8f148149168dc7dede141
Salam4nder <kamran.personal@icloud.com>
2023-03-29 22:07:48 +0200

    Add payload

---
+++ pkg/token/payload.go
@@ -0,0 +1,43 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Different types of error returned by the VerifyToken function.
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payload data of the token.
+type Payload struct {
+	ID        primitive.ObjectID `json:"id"`
+	Email     string             `json:"username"`
+	IssuedAt  time.Time          `json:"issued_at"`
+	ExpiredAt time.Time          `json:"expired_at"`
+}
+
+// NewPayload creates a new token payload with a specific email and duration.
+func NewPayload(email string, duration time.Duration) (*Payload, error) {
+	tokenID := primitive.NewObjectID()
+
+	payload := &Payload{
+		ID:        tokenID,
+		Email:     email,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
+
+// Valid checks if the token payload is valid.
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
