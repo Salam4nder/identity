72a93ead5b3fe33dc7a6a00a49c71f02b71a4369
Salam4nder <kamran.personal@icloud.com>
2023-04-10 18:19:36 +0200

    Add gateway

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -1,11 +1,15 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 
 	"github.com/Salam4nder/user/internal/config"
-	"github.com/Salam4nder/user/internal/proto/pb"
+	"github.com/Salam4nder/user/internal/proto/gen"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -13,37 +17,70 @@ import (
 )
 
 type server struct {
-	srvc   *userService
-	cfg    *config.GRPCServer
-	logger *zap.Logger
+	userSrvc *userService
+	cfg      *config.Server
+	logger   *zap.Logger
 }
 
 // NewServer creates new gRPC server.
 func NewServer(
 	srvc *userService,
-	cfg *config.GRPCServer,
+	cfg *config.Server,
 	logger *zap.Logger,
 ) *server {
 	return &server{
-		srvc:   srvc,
-		cfg:    cfg,
-		logger: logger,
+		userSrvc: srvc,
+		cfg:      cfg,
+		logger:   logger,
 	}
 }
 
-// Serve starts the gRPC server.
-func (s *server) Serve() error {
-	listener, err := net.Listen("tcp", s.cfg.Addr())
+// ServeGRPC starts the gRPC server.
+func (s *server) ServeGRPC() error {
+	listener, err := net.Listen("tcp", s.cfg.GRPCAddr())
 	if err != nil {
 		return fmt.Errorf("failed to listen: %w", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterUserServer(grpcServer, s.srvc)
+	gen.RegisterUserServer(grpcServer, s.userSrvc)
 	reflection.Register(grpcServer)
 
 	s.logger.Info(
-		"gRPC server is running", zap.String("address", s.cfg.Addr()))
+		"gRPC server is running", zap.String("address", s.cfg.GRPCAddr()))
 
 	return grpcServer.Serve(listener)
+}
+
+
+// ServeGRPCGateway starts the gRPC gateway.
+func (s *server) ServeGRPCGateway() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gen.RegisterUserHandlerFromEndpoint(ctx, mux, s.cfg.GRPCAddr(), opts)
+	if err != nil {
+		log.Fatalf("failed to register gateway: %v", err)
+	}
+
+	server := &http.Server{
+		Handler: mux,
+	}
+
+	listener, err := net.Listen("tcp", s.cfg.HTTPAddr())
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s.logger.Info(
+		"gRPC gateway is running", zap.String("address", s.cfg.HTTPAddr()))
+
+	err = server.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
