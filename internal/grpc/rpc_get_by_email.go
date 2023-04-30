429fb9805d456ef62eb403f755e529bc91f9b05c
Salam4nder <kamran.personal@icloud.com>
2023-04-30 20:05:30 +0200

    Swapping DB to PSQL

---
+++ internal/grpc/rpc_get_by_email.go
@@ -0,0 +1,40 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Salam4nder/user/internal/db"
+	"github.com/Salam4nder/user/internal/proto/gen"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// GetByEmail returns a user by email. Returns an error if the user couldn't be not
+// found or if the request is invalid.
+func (s *userServer) GetByEmail(
+	ctx context.Context, req *gen.GetByEmailRequest) (*gen.UserResponse, error) {
+	if req == nil {
+		return nil, requestIsNilError()
+	}
+
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email can not be empty")
+	}
+
+	user, err := s.storage.ReadUserByEmail(ctx, req.GetEmail())
+	if err != nil {
+		switch {
+		case errors.Is(err, db.ErrUserNotFound):
+			return nil, status.Error(codes.NotFound, err.Error())
+
+		default:
+			s.logger.Error("failed to find user", zap.Error(err))
+			return nil, internalServerError()
+		}
+	}
+
+	return userToProtoResponse(user), nil
+}
