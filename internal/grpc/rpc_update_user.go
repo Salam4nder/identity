34dc3d9952184f4645b96ba9895ad92c59effc0e
Salam4nder <kamran.personal@icloud.com>
2023-04-30 20:06:01 +0200

    Swapping DB to PSQL

---
+++ internal/grpc/rpc_update_user.go
@@ -0,0 +1,85 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Salam4nder/user/internal/db"
+	"github.com/Salam4nder/user/internal/proto/gen"
+	"github.com/Salam4nder/user/pkg/util"
+	"github.com/google/uuid"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// UpdateUser updates a user by id. Returns an error if the user couldn't be updated
+// or if the request is invalid.
+func (s *userServer) UpdateUser(
+	ctx context.Context, req *gen.UpdateUserRequest) (*gen.UserResponse, error) {
+	authPayload, err := s.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError()
+	}
+
+	if err := validateUpdateUserRequest(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	if authPayload.Email != req.GetEmail() {
+		return nil, status.Errorf(
+			codes.PermissionDenied, "only owner can update details")
+	}
+
+	id, err := uuid.Parse(req.GetId())
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "ID is invalid")
+	}
+
+	params := db.UpdateUserParams{
+		ID:       id,
+		FullName: req.GetFullName(),
+		Email:    req.GetEmail(),
+	}
+
+	updatedUser, err := s.storage.UpdateUserTx(ctx, params)
+	if err != nil {
+		switch {
+		case errors.Is(err, db.ErrUserNotFound):
+			return nil, status.Error(codes.NotFound, err.Error())
+
+		default:
+			s.logger.Error("failed to find user", zap.Error(err))
+			return nil, internalServerError()
+		}
+	}
+
+	return userToProtoResponse(updatedUser), nil
+}
+
+// validateUpdateUserRequest returns nil if the request is valid.
+func validateUpdateUserRequest(req *gen.UpdateUserRequest) error {
+	if req == nil {
+		return errors.New("request can not be nil")
+	}
+
+	if req.Id == "" {
+		return errors.New("ID can not be empty")
+	}
+
+	var (
+		fullNameErr error
+		emailErr    error
+	)
+
+	if err := util.ValidateFullName(req.GetFullName()); err != nil {
+		fullNameErr = err
+	}
+
+	if err := util.ValidateEmail(req.GetEmail()); err != nil {
+		emailErr = err
+	}
+
+	return errors.Join(fullNameErr, emailErr)
+}
