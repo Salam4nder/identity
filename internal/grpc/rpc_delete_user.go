f9a437f4b0c3dee69cd37630fc16dc2455675996
Salam4nder <kamran.personal@icloud.com>
2023-04-30 20:05:20 +0200

    Swapping DB to PSQL

---
+++ internal/grpc/rpc_delete_user.go
@@ -0,0 +1,47 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Salam4nder/user/internal/db"
+	"github.com/Salam4nder/user/internal/proto/gen"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// DeleteUser deletes a user by id. Returns an error if the user couldn't be deleted,
+// if the user doesn't exist or if the request is invalid.
+func (s *userServer) DeleteUser(
+	ctx context.Context, req *gen.UserID) (*emptypb.Empty, error) {
+	if req == nil {
+		return nil, requestIsNilError()
+	}
+
+	if req.GetId() == "" {
+		return nil, status.Error(codes.InvalidArgument, "ID can not be empty")
+	}
+
+	id, err := uuid.Parse(req.GetId())
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "ID is invalid")
+	}
+
+	err = s.storage.DeleteUserTx(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, db.ErrUserNotFound):
+			return nil, status.Error(codes.NotFound, err.Error())
+
+		default:
+			s.logger.Error("failed to find user", zap.Error(err))
+			return nil, internalServerError()
+		}
+	}
+
+	return &emptypb.Empty{}, nil
+}
