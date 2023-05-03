5de8ed54cca652886e5938a1b200a7a63f007224
Salam4nder <kamran.personal@icloud.com>
2023-05-03 21:31:49 +0200

    Add HTTP logger

--- pkg/grpc/logger.go
+++ pkg/grpc/logger.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -27,14 +28,60 @@ func LoggerInterceptor(
 		code = status.Code()
 	}
 
+	logger := log.Info()
+
+    if code != codes.OK {
+        logger = log.Error().Err(err)
+    }
+
 	duration := time.Since(startTime)
 
-	log.Info().
+	logger.Str("protocol", "grpc").
 		Str("method", info.FullMethod).
 		Int("status_code", int(code)).
 		Str("status_text", code.String()).
-		Dur("duration", duration).
-		Msg("gRPC request received")
+		Dur("duration", duration)
 
 	return result, err
+}
+
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = body
+	return rec.ResponseWriter.Write(body)
+}
+
+// HTTPLogger logs HTTP requests. Used for gRPC Gateway.
+func HTTPLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		startTime := time.Now()
+		rec := &ResponseRecorder{
+			ResponseWriter: res,
+			StatusCode:     http.StatusOK,
+		}
+		handler.ServeHTTP(rec, req)
+		duration := time.Since(startTime)
+
+		logger := log.Info()
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("body", rec.Body)
+		}
+
+		logger.Str("protocol", "http").
+			Str("method", req.Method).
+			Str("path", req.RequestURI).
+			Int("status_code", rec.StatusCode).
+			Str("status_text", http.StatusText(rec.StatusCode)).
+			Dur("duration", duration)
+	})
 }
