308b491c9a08bf490224bc9acf29989fc82bb35b
Salam4nder <kamran.personal@icloud.com>
2023-09-09 20:54:31 +0200

    Formatting

--- internal/grpc/user_server.go
+++ internal/grpc/user_server.go
@@ -5,9 +5,11 @@ import (
 	"github.com/Salam4nder/user/internal/db"
 	"github.com/Salam4nder/user/internal/proto/gen"
 	"github.com/Salam4nder/user/pkg/token"
+
 	"github.com/rs/zerolog"
 )
 
+// UserServer is a gRPC server for user service.
 type userServer struct {
 	gen.UserServer
 
@@ -21,7 +23,8 @@ type userServer struct {
 func NewUserService(
 	store *db.SQL,
 	log *zerolog.Logger,
-	cfg config.UserService) (*userServer, error) {
+	cfg config.UserService,
+) (*userServer, error) {
 	tokenMaker, err := token.NewPasetoMaker(cfg.SymmetricKey)
 	if err != nil {
 		return nil, err
