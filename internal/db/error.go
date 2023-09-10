05ab6105403c37a6249d54df9fec38ebd4cb8dd3
Salam4nder <kamran.personal@icloud.com>
2023-09-10 15:11:28 +0200

    Add better sentinel err handling and tests

--- internal/db/error.go
+++ internal/db/error.go
@@ -1,9 +1,49 @@
 package db
 
-import "errors"
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/lib/pq"
+)
 
 // Common db errors.
 var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 	ErrUserNotFound   = errors.New("user not found")
 )
+
+// IsSentinelErr is a sentinel error.
+func IsSentinelErr(err error) bool {
+	if errors.Is(err, sql.ErrNoRows) {
+		return true
+	}
+
+	var pqErr *pq.Error
+
+	if errors.As(err, &pqErr) {
+		return true
+	}
+
+	return false
+}
+
+// SentinelErr returns a sentinel error.
+func SentinelErr(err error) error {
+	if err == sql.ErrNoRows {
+		return ErrUserNotFound
+	}
+
+	var pqErr *pq.Error
+
+	pqErr = err.(*pq.Error)
+
+	switch pqErr.Code.Name() {
+	case "unique_violation":
+		return ErrDuplicateEmail
+
+	// TODO: Add more cases here.
+	default:
+		return err
+	}
+}
