9b22afb39f77aea12d3ff701a1113f0340364292
Salam4nder <kamran.personal@icloud.com>
2023-10-21 13:52:52 +0200

    Remove parallel tests in error_test

--- internal/db/error_test.go
+++ internal/db/error_test.go
@@ -11,19 +11,13 @@ import (
 const PQErrUniqueViolationCode = "23505"
 
 func Test_IsSentinelErr(t *testing.T) {
-	t.Parallel()
-
 	t.Run("sql.ErrNoRows returns true", func(t *testing.T) {
-		t.Parallel()
-
 		if !IsSentinelErr(sql.ErrNoRows) {
 			t.Error("sql.ErrNoRows should be a sentinel error")
 		}
 	})
 
 	t.Run("pq.Error returns true", func(t *testing.T) {
-		t.Parallel()
-
 		err := &pq.Error{
 			Code: "unique_violation",
 		}
@@ -34,8 +28,6 @@ func Test_IsSentinelErr(t *testing.T) {
 	})
 
 	t.Run("non-sentinel error returns false", func(t *testing.T) {
-		t.Parallel()
-
 		err := errors.New("some error")
 
 		if IsSentinelErr(err) {
@@ -45,19 +37,13 @@ func Test_IsSentinelErr(t *testing.T) {
 }
 
 func Test_SentinelErr(t *testing.T) {
-	t.Parallel()
-
 	t.Run("sql.ErrNoRows returns ErrUserNotFound", func(t *testing.T) {
-		t.Parallel()
-
 		if !errors.Is(SentinelErr(sql.ErrNoRows), ErrUserNotFound) {
 			t.Error("sql.ErrNoRows should be ErrUserNotFound")
 		}
 	})
 
 	t.Run("pq.Error returns ErrDuplicateEmail", func(t *testing.T) {
-		t.Parallel()
-
 		err := &pq.Error{
 			Code: PQErrUniqueViolationCode,
 		}
@@ -68,8 +54,6 @@ func Test_SentinelErr(t *testing.T) {
 	})
 
 	t.Run("non-specified sentinel error returns itself", func(t *testing.T) {
-		t.Parallel()
-
 		err := &pq.Error{
 			Code: "some_other_code",
 		}
