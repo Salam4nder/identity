1ab7e36decff39cc7e0bab42a113676d539ac639
Salam4nder <kamran.personal@icloud.com>
2023-10-21 13:53:41 +0200

    Add db CreateUser tests

--- internal/db/user_test.go
+++ internal/db/user_test.go
@@ -4,17 +4,21 @@
 package db
 
 import (
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSQL_CreateUser(t *testing.T) {
 	tests := []struct {
-		name    string
-		params  CreateUserParams
-		wantErr bool
+		name        string
+		params      CreateUserParams
+		wantErr     bool
+		requiredErr error
+		preInsert   func() error
 	}{
 		{
 			name: "Success",
@@ -22,21 +26,91 @@ func TestSQL_CreateUser(t *testing.T) {
 				FullName:  "Kam Gam",
 				Email:     "email@test.com",
 				Password:  "password",
-				CreatedAt: time.Now(),
+				CreatedAt: time.Now().UTC(),
 			},
 		},
+		{
+			name: "Duplicate email returns error",
+			params: CreateUserParams{
+				FullName:  "Kam Gam",
+				Email:     "email@test.com",
+				Password:  "password",
+				CreatedAt: time.Now().UTC(),
+			},
+			preInsert: func() error {
+				_, err := TestSQLConnPool.db.ExecContext(
+					ctx,
+					`INSERT INTO users (
+                       full_name,
+                       email,
+                       password_hash,
+                       created_at
+                   ) VALUES (
+                       'Kam Gam',
+                       'email@test.com',
+                       'password',
+                       timezone('utc', now())
+                       )`,
+				)
+				return err
+			},
+			wantErr:     true,
+			requiredErr: ErrDuplicateEmail,
+		},
+		{
+			name: "Name exceeds 255 chars returns err",
+			params: CreateUserParams{
+				FullName:  strings.Repeat("a", 256),
+				Email:     "email@test.com",
+				Password:  "password",
+				CreatedAt: time.Now().UTC(),
+			},
+			wantErr:     true,
+			requiredErr: ErrStringTooLong,
+		},
+		{
+			name: "Email exceeds 255 chars returns err",
+			params: CreateUserParams{
+				FullName:  "Full name",
+				Email:     strings.Repeat("a", 256),
+				Password:  "password",
+				CreatedAt: time.Now().UTC(),
+			},
+			wantErr:     true,
+			requiredErr: ErrStringTooLong,
+		},
 	}
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			if test.preInsert != nil {
+				require.NoError(t, test.preInsert())
+			}
+
 			got, err := TestSQLConnPool.CreateUser(ctx, test.params)
 			if test.wantErr {
 				require.Error(t, err)
+				if test.requiredErr != nil {
+					require.ErrorIs(t, err, test.requiredErr)
+				}
 			} else {
 				require.NoError(t, err)
 				require.NotNil(t, got)
 				require.NotEmpty(t, got.ID)
+				require.NotEqual(t, got.ID, uuid.Nil)
 				require.Equal(t, test.params.FullName, got.FullName)
+				require.Equal(t, test.params.Email, got.Email)
+				require.Equal(t, test.params.CreatedAt, got.CreatedAt)
 				require.NotEmpty(t, got.PasswordHash)
+
+				t.Cleanup(func() {
+					_, err := TestSQLConnPool.db.ExecContext(
+						ctx,
+						`DELETE FROM users WHERE id = $1`,
+						got.ID,
+					)
+					require.NoError(t, err)
+				})
 			}
 		})
 	}
