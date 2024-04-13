e6f85f4b5450339477b40760890c9fbc73c81b12
Salam4nder <kamran.personal@icloud.com>
2024-04-13 21:25:20 +0200

    refactor: move main out of cmd

--- main.go
+++ main.go
@@ -22,6 +22,13 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/stimtech/go-migration"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -52,25 +59,25 @@ func main() {
 	cfg, err := config.New()
 	exitWithError(err)
 
-	// TODO: config this better.
-	redisOpt := asynq.RedisClientOpt{
-		Addr: cfg.Redis.Addr(),
-	}
-
 	// UNIX Time is faster and smaller than most timestamps
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	if cfg.Environment == EnvironmentDev {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	}
 
+	otelShutdown, err := setupOTelSDK(ctx)
+
 	db, err := db.NewSQLDatabase(ctx, cfg.PSQL)
 	exitWithError(err)
-
 	migration := migration.New(db.DB(), zap.NewNop())
 	if err = migration.Migrate(); err != nil {
 		exitWithError(err)
 	}
 
+	// TODO: config this better.
+	redisOpt := asynq.RedisClientOpt{
+		Addr: cfg.Redis.Addr(),
+	}
 	taskCreator := task.NewRedisTaskCreator(redisOpt)
 	taskProcessor := task.NewRedisTaskProcessor(db, redisOpt)
 	go func() {
@@ -81,11 +88,10 @@ func main() {
 
 	userServer, err := internalGRPC.NewUserServer(db, taskCreator, cfg.UserService)
 	exitWithError(err)
-
 	grpcListener, err := net.Listen("tcp", cfg.Server.GRPCAddr())
 	exitWithError(err)
-
 	grpcServer := grpc.NewServer(
+		// grpc.StatsHandler(otelgrpc.NewServerHandler()),
 		grpc.ChainUnaryInterceptor(
 			grpcUtil.LoggerInterceptor,
 			recovery.UnaryServerInterceptor(),
@@ -133,17 +139,19 @@ func main() {
 		Str("address", cfg.Server.HTTPAddr()).
 		Msg("gRPC gateway is running")
 
-	// Wait for interrupt signal to gracefully shutdown the server with.
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 	<-quit
 
-	log.Info().Msg("shutting down server...")
+	log.Info().Msg("signal received, shutting down...")
 	grpcServer.GracefulStop()
 	if err := server.Shutdown(ctx); err != nil {
 		exitWithError(err)
 	}
-	log.Info().Msg("server gracefully stopped")
+	if err := otelShutdown(ctx); err != nil {
+		exitWithError(err)
+	}
+	log.Info().Msg("service gracefully stopped")
 }
 
 func exitWithError(err error) {
@@ -151,4 +159,84 @@ func exitWithError(err error) {
 		log.Error().Err(err).Msg("main: exit with error")
 		os.Exit(1)
 	}
+}
+
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	meterProvider, err := newMeterProvider()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	return
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider() (*trace.TracerProvider, error) {
+	traceExporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		return nil, err
+	}
+	grpcExporter, err := otlptracegrpc.New(context.Background())
+
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(traceExporter),
+		trace.WithBatcher(grpcExporter),
+	)
+	return traceProvider, nil
+}
+
+func newMeterProvider() (*metric.MeterProvider, error) {
+	metricExporter, err := stdoutmetric.New()
+	if err != nil {
+		return nil, err
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(metricExporter)),
+	)
+	return meterProvider, nil
 }
