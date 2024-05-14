4036b605f21aa90b88584b047acef4e67286bd5c
Salam4nder <kamran.personal@icloud.com>
2024-05-14 20:33:29 +0200

    feat: rpc tracing

--- internal/grpc/rpc_delete_user.go
+++ internal/grpc/rpc_delete_user.go
@@ -3,10 +3,12 @@ package grpc
 import (
 	"context"
 	"errors"
+	"log/slog"
 
 	"github.com/Salam4nder/user/internal/db"
 	"github.com/Salam4nder/user/internal/grpc/gen"
 	"github.com/google/uuid"
+	otelCode "go.opentelemetry.io/otel/codes"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -14,8 +16,24 @@ import (
 
 // DeleteUser deletes a user by ID.
 func (x *UserServer) DeleteUser(ctx context.Context, req *gen.UserID) (*emptypb.Empty, error) {
+	var err error
+	ctx, span := tracer.Start(ctx, "rpc.DeleteUser")
+	defer func() {
+		if err != nil {
+			span.SetStatus(otelCode.Error, err.Error())
+			span.RecordError(err)
+		}
+		span.End()
+	}()
 	if req == nil {
 		return nil, requestIsNilError()
+	}
+
+	attrs, err := GenSpanAttributes(req)
+	if err == nil {
+		span.SetAttributes(attrs...)
+	} else {
+		slog.Warn("DeleteUser: GenSpanAttributes", "err", err)
 	}
 
 	if req.GetId() == "" {
@@ -33,7 +51,7 @@ func (x *UserServer) DeleteUser(ctx context.Context, req *gen.UserID) (*emptypb.
 			return nil, status.Error(codes.NotFound, err.Error())
 
 		default:
-			return nil, internalServerError(err)
+			return nil, internalServerError()
 		}
 	}
 
