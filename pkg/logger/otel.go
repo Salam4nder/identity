bb748a1cb771067c965b2979cafdd8a7bf0f5c49
Salam4nder <kamran.personal@icloud.com>
2024-05-14 20:34:36 +0200

    feat: slog otel handler

--- pkg/logger/otel.go
+++ pkg/logger/otel.go
@@ -3,28 +3,55 @@ package logger
 import (
 	"context"
 	"log/slog"
+
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
-type OtelLogger struct {
-	l *slog.Logger
+type OtelHandler struct {
+	next slog.Handler
 }
 
-func NewOtelLogger(l *slog.Logger) slog.Handler {
-	return &OtelLogger{l: l}
+func NewOtelHandler(n slog.Handler) slog.Handler {
+	return &OtelHandler{next: n}
 }
 
-func (x *OtelLogger) Enabled(ctx context.Context, lvl slog.Level) bool {
-	return x.l.Enabled(ctx, lvl)
+func (x *OtelHandler) Enabled(ctx context.Context, lvl slog.Level) bool {
+	return x.next.Enabled(ctx, lvl)
 }
 
-func (x *OtelLogger) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return x.l.Handler().WithAttrs(attrs)
+func (x *OtelHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return x.next.WithAttrs(attrs)
 }
 
-func (x *OtelLogger) WithGroup(name string) slog.Handler {
-	return x.l.Handler().WithGroup(name)
+func (x *OtelHandler) WithGroup(name string) slog.Handler {
+	return x.next.WithGroup(name)
 }
 
-func (x *OtelLogger) Handle(ctx context.Context, r slog.Record) error {
-	return nil
+func (x *OtelHandler) Handle(ctx context.Context, r slog.Record) error {
+	if ctx == nil {
+		return x.next.Handle(ctx, r)
+	}
+
+	span := trace.SpanFromContext(ctx)
+	if span == nil || !span.IsRecording() {
+		return x.next.Handle(ctx, r)
+	}
+
+	spanContext := span.SpanContext()
+	if spanContext.HasTraceID() {
+		traceID := spanContext.TraceID().String()
+		r.AddAttrs(slog.String("trace_id", traceID))
+	}
+
+	if spanContext.HasSpanID() {
+		spanID := spanContext.SpanID().String()
+		r.AddAttrs(slog.String("span_id", spanID))
+	}
+
+	if r.Level >= slog.LevelError {
+		span.SetStatus(codes.Error, r.Message)
+	}
+
+	return x.next.Handle(ctx, r)
 }
