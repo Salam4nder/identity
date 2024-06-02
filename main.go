3855226274b9abaeb255be9688d6a3324698b65d
Salam4nder <kamran.personal@icloud.com>
2024-06-02 20:53:24 +0200

    refactor: move otel setup to its own package

--- main.go
+++ main.go
@@ -16,18 +16,11 @@ import (
 	internalGRPC "github.com/Salam4nder/user/internal/grpc"
 	"github.com/Salam4nder/user/internal/grpc/gen"
 	"github.com/Salam4nder/user/internal/grpc/interceptors"
+	"github.com/Salam4nder/user/internal/otel"
 	"github.com/Salam4nder/user/pkg/logger"
 	"github.com/google/uuid"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	"github.com/stimtech/go-migration"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/exporters/prometheus"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
@@ -66,7 +59,11 @@ func main() {
 		slog.SetDefault(slog.New(logger.NewOtelHandler(slog.NewJSONHandler(os.Stdout, nil))))
 	}
 
-	otelShutdown, err := setupOTELSDK(ctx)
+	otelShutdown, err := otel.Setup(ctx, otel.SetupOpts{
+		ServiceName:    serviceName,
+		ServiceVersion: serviceVersion,
+		ServiceID:      serviceID,
+	})
 	defer func() {
 		err = errors.Join(err, otelShutdown(context.Background()))
 	}()
@@ -110,7 +107,7 @@ func main() {
 	go func() {
 		srvErrChan <- grpcServer.Serve(grpcListener)
 	}()
-	slog.InfoContext(ctx, "gRPC server is running", "address", cfg.Server.GRPCAddr())
+	slog.InfoContext(ctx, "main: gRPC server is running", "address", cfg.Server.GRPCAddr())
 
 	select {
 	case err := <-srvErrChan:
@@ -119,7 +116,7 @@ func main() {
 		slog.InfoContext(ctx, "main: context done, shutting down...")
 	}
 	grpcServer.GracefulStop()
-	slog.InfoContext(ctx, "gRPC server stopped")
+	err = errors.Join(err, db.Close())
 
 	if err != nil {
 		slog.ErrorContext(ctx, "main: error upon exit", "error", err)
@@ -132,98 +129,4 @@ func exitOnError(ctx context.Context, err error) {
 		ctx.Done()
 		os.Exit(1)
 	}
-}
-
-// setupOTELSDK bootstraps the OpenTelemetry pipeline.
-// If it does not return an error, make sure to call shutdown for proper cleanup.
-func setupOTELSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
-	var shutdownFuncs []func(context.Context) error
-
-	// shutdown calls cleanup functions registered via shutdownFuncs.
-	// The errors from the calls are joined.
-	// Each registered cleanup will be invoked once.
-	shutdown = func(ctx context.Context) error {
-		var err error
-		for _, fn := range shutdownFuncs {
-			err = errors.Join(err, fn(ctx))
-		}
-		shutdownFuncs = nil
-		return err
-	}
-
-	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
-	handleErr := func(inErr error) {
-		err = errors.Join(inErr, shutdown(ctx))
-	}
-
-	// Set up propagator.
-	prop := newPropagator()
-	otel.SetTextMapPropagator(prop)
-
-	// Set up trace provider.
-	tracerProvider, err := newTraceProvider(ctx)
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
-	otel.SetTracerProvider(tracerProvider)
-
-	// Set up meter provider.
-	meterProvider, err := newMeterProvider()
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
-	otel.SetMeterProvider(meterProvider)
-
-	return
-}
-
-func newPropagator() propagation.TextMapPropagator {
-	return propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{},
-		propagation.Baggage{},
-	)
-}
-
-func newTraceProvider(ctx context.Context) (*trace.TracerProvider, error) {
-	grpcExporter, err := otlptracegrpc.New(
-		ctx,
-		otlptracegrpc.WithInsecure(),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	res := resource.NewWithAttributes(
-		semconv.SchemaURL,
-		semconv.ServiceNameKey.String(serviceName),
-		semconv.ServiceVersionKey.String(serviceVersion),
-		semconv.ServiceInstanceIDKey.String(serviceID),
-	)
-
-	traceProvider := trace.NewTracerProvider(
-		trace.WithBatcher(grpcExporter),
-		trace.WithResource(res),
-	)
-	return traceProvider, nil
-}
-
-func newMeterProvider() (*metric.MeterProvider, error) {
-	prom, err := prometheus.New()
-	if err != nil {
-		return nil, err
-	}
-
-	meterProvider := metric.NewMeterProvider(
-		metric.WithReader(prom),
-		metric.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(serviceName),
-			semconv.ServiceInstanceIDKey.String(serviceVersion),
-		)),
-	)
-	return meterProvider, nil
 }
