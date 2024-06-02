573e6f6ef861a0dd7dc8fd9e8f6d13904bfb6e00
Salam4nder <kamran.personal@icloud.com>
2024-06-02 22:31:14 +0200

    feat: event package

---
+++ internal/event/user.go
@@ -0,0 +1,3 @@
+package event
+
+const UserRegistered = "user_registered"
---
+++ internal/event/worker.go
@@ -0,0 +1,45 @@
+package event
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"log/slog"
+
+	"github.com/Salam4nder/user/internal/email"
+	"github.com/nats-io/nats.go"
+)
+
+type Worker struct {
+	mailSender email.Sender
+}
+
+func NewWorker(sender email.Sender) *Worker {
+	return &Worker{
+		mailSender: sender,
+	}
+}
+
+func (x *Worker) Work(ctx context.Context, natsCh chan *nats.Msg) {
+	for {
+		select {
+		case <-ctx.Done():
+			slog.InfoContext(ctx, "event: context done, shutting down...")
+			return
+		case msg := <-natsCh:
+			var m email.Email
+			if err := gob.NewDecoder(bytes.NewReader(msg.Data)).Decode(&m); err != nil {
+				slog.WarnContext(ctx, "event: failed to decode message", "error", err)
+				msg.Ack()
+				continue
+			}
+			x.mailSender.SendEmail(ctx, email.Email{
+				To:      m.To,
+				Subject: m.Subject,
+				Body:    m.Body,
+				From:    m.From,
+			})
+			msg.Ack()
+		}
+	}
+}
