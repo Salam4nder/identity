256a3df2d526234e8a32dfd85207f66bb4d28393
Salam4nder <kamran.personal@icloud.com>
2024-06-02 22:31:18 +0200

    feat: email package

---
+++ internal/email/email.go
@@ -0,0 +1,45 @@
+package email
+
+import (
+	"context"
+	"log/slog"
+)
+
+const (
+	// TODO(kg): Remove this.
+	TestSubject = "You have requested to create an identity."
+	TestBody    = "Do the following step to verify your identity."
+	TestFrom    = "fugaziindustries@proton.me"
+)
+
+type Email struct {
+	To      string
+	Subject string
+	Body    string
+	From    string
+}
+
+type Sender interface {
+	SendEmail(ctx context.Context, email Email) error
+}
+
+// NoOpSender is a no-op implementation of the Sender interface.
+// It logs a fake email send to the console.
+type NoOpSender struct{}
+
+func NewNoOpSender() *NoOpSender {
+	return &NoOpSender{}
+}
+
+// SendEmail logs the email to the console.
+func (x *NoOpSender) SendEmail(ctx context.Context, email Email) error {
+	slog.InfoContext(
+		ctx,
+		"no-op mailer: sending email",
+		"to", email.To,
+		"subject", email.Subject,
+		"body", email.Body,
+		"from", email.From,
+	)
+	return nil
+}
