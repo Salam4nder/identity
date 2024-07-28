7e2824f9cabcafb21c0a08f001bb97d77caad7bc
Salam4nder <kamran.personal@icloud.com>
2024-07-28 19:14:10 +0200

    feat: db.InputError

--- internal/db/error.go
+++ internal/db/error.go
@@ -3,11 +3,11 @@ package db
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 
 	"github.com/lib/pq"
 )
 
-// Common db errors.
 var (
 	ErrDuplicateEmail       = errors.New("db: duplicate email")
 	ErrStringTooLong        = errors.New("db: string too long")
@@ -15,6 +15,17 @@ var (
 	ErrNoRowsAffected       = errors.New("db: no rows affected")
 	ErrMultipleRowsAffected = errors.New("db: multiple rows affected")
 )
+
+// InputError is returned in case of input errors that are meant to be
+// sent back to the user.
+type InputError struct {
+	Field string
+	Value any
+}
+
+func (x InputError) Error() string {
+	return fmt.Sprintf("input error on %s with value %v", x.Field, x.Value)
+}
 
 // IsSentinelErr is a sentinel error.
 func IsSentinelErr(err error) bool {
