3157f990f951be2952b073898309c235b1f0afb5
Salam4nder <kamran.personal@icloud.com>
2024-08-03 15:37:34 +0200

    feat: credentials auth strategy

--- internal/auth/strategy/credentials.go
+++ internal/auth/strategy/credentials.go
@@ -2,18 +2,12 @@ package strategy
 
 import (
 	"context"
-
-	"github.com/Salam4nder/user/internal/auth"
 )
 
 type (
-	Authenticator struct{}
-	Input         struct{}
-	Output        struct{}
+	Credentials struct{}
 )
 
-var _ auth.Strategy = (*Authenticator)(nil)
+// var _ auth.Strategy = (*Credentials)(nil)
 
-func (x Authenticator) Authenticate(ctx context.Context, in Input) (Output, error)
-
-func (x Input) Valid() error { return nil }
+func (x Credentials) Authenticate(ctx context.Context) error
