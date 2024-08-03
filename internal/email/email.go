0d896b5428e569a9f5262bc6c548e92ede5d9af4
Salam4nder <kamran.personal@icloud.com>
2024-08-03 22:42:58 +0200

    feat: email ingestor

--- internal/email/email.go
+++ internal/email/email.go
@@ -1,26 +1,56 @@
 package email
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"log/slog"
 
+	"github.com/nats-io/nats.go"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
+var tracer = otel.Tracer("email")
+
 const (
+	IngestedEvent = "email_ingested"
+
 	// TODO(kg): Remove this.
 	TestSubject = "You have requested to create an identity."
 	TestBody    = "Do the following step to verify your identity."
 	TestFrom    = "fugaziindustries@proton.me"
 )
 
-var tracer = otel.Tracer("email")
-
 type Email struct {
 	To      string
 	Subject string
 	Body    string
 	From    string
+}
+
+func (x Email) TraceAttributes() []attribute.KeyValue {
+	return []attribute.KeyValue{
+		attribute.String("to", x.To),
+		attribute.String("subject", x.Subject),
+		attribute.String("Body", x.Body),
+		attribute.String("From", x.From),
+	}
+}
+
+func Ingest(ctx context.Context, natsConn *nats.Conn, email Email) error {
+	ctx, span := tracer.Start(ctx, "Ingest", trace.WithAttributes(email.TraceAttributes()...))
+	defer span.End()
+
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(email); err != nil {
+		return err
+	}
+	if err := natsConn.Publish(IngestedEvent, buf.Bytes()); err != nil {
+		return err
+	}
+	return nil
 }
 
 type Sender interface {
@@ -37,7 +67,7 @@ func NewNoOpSender() *NoOpSender {
 
 // SendEmail logs the email to the console.
 func (x *NoOpSender) SendEmail(ctx context.Context, email Email) error {
-	ctx, span := tracer.Start(ctx, "SendEmail")
+	ctx, span := tracer.Start(ctx, "SendEmail", trace.WithAttributes(email.TraceAttributes()...))
 	defer span.End()
 
 	slog.InfoContext(
