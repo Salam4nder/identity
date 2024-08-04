461e11357e9e246d80fad42e1ab869f1d8c393a0
Salam4nder <kamran.personal@icloud.com>
2024-08-04 19:59:14 +0200

    refactor: rename db -> database

--- internal/db/migrations/2023-05-29-init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS credentials (
-    id uuid PRIMARY KEY,
-    email varchar(255) NOT NULL UNIQUE,
-    password_hash varchar(255) NOT NULL,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz DEFAULT NULL
-);
