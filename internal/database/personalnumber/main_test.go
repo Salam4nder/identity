83cbe1d5259bd406d56d7fca99621c8ac951a233
Salam4nder <kamran.personal@icloud.com>
2024-08-05 22:05:24 +0200

    feat: personalnumber db tests

---
+++ internal/database/personalnumber/main_test.go
@@ -0,0 +1,48 @@
+//go:build testdb
+// +build testdb
+
+package personalnumber_test
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Salam4nder/identity/internal/config"
+	"github.com/Salam4nder/identity/internal/database/personalnumber"
+)
+
+var testConn *sql.DB
+
+func Conn() (*sql.DB, func()) {
+	return testConn, func() {
+		_, err := testConn.Exec(fmt.Sprintf("TRUNCATE %s CASCADE", personalnumber.Tablename))
+		if err != nil {
+			slog.Error(fmt.Sprintf("truncating table %s", personalnumber.Tablename), "err", err)
+		}
+	}
+}
+
+func TestMain(m *testing.M) {
+	cfg := config.PSQLTestConfig()
+
+	db, err := sql.Open(cfg.Driver(), cfg.Addr())
+	if err != nil {
+		slog.Error("database: opening sql", "err", err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		slog.Error("database: pinging", "err", err)
+		os.Exit(1)
+	}
+
+	testConn = db
+	os.Exit(m.Run())
+}
