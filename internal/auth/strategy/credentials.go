563be5d2833dd6b08f25006a4a01627017e46a73
Salam4nder <kamran.personal@icloud.com>
2024-08-31 19:02:15 +0200

    feat: credentials from context

--- internal/auth/strategy/credentials.go
+++ internal/auth/strategy/credentials.go
@@ -3,6 +3,7 @@ package strategy
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"time"
 	"unicode/utf8"
@@ -11,41 +12,31 @@ import (
 	"github.com/Salam4nder/identity/internal/email"
 	"github.com/Salam4nder/identity/pkg/password"
 	"github.com/Salam4nder/identity/pkg/validation"
-	"github.com/Salam4nder/identity/proto/gen"
 	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/trace"
 )
 
-var tracer = otel.Tracer("strategy")
+var (
+	tracer = otel.Tracer("strategy")
 
-const TypeCredentials = "Credentials"
+	credentialsKey ctxKey
+)
 
 type (
+	ctxKey int
+
 	// Credentials implements the [Strategy] interface and has everything
 	// to be able to [Register], [Authenticate] and [Revoke] with credentials.
 	Credentials struct {
 		db       *sql.DB
 		natsConn *nats.Conn
-
-		email    string
-		password password.SafeString
-	}
-
-	// CredentialsInput is the input for the credentials strategy.
-	CredentialsInput struct {
-		Email    string
-		Password string
 	}
 )
 
-func (x CredentialsInput) TraceAttributes() []attribute.KeyValue {
-	return []attribute.KeyValue{
-		attribute.String("email", x.Email),
-		attribute.Int("password length", utf8.RuneCountInString((x.Password))),
-	}
+type Input struct {
+	Email, Password string
 }
 
 // NewCredentials creates a new [Credentials] strategy for authentication.
@@ -55,28 +46,16 @@ func NewCredentials(db *sql.DB, natsConn *nats.Conn) *Credentials {
 	return &Credentials{db: db, natsConn: natsConn}
 }
 
-func (x *Credentials) ConfiguredStrategy() gen.Strategy {
-	return gen.Strategy_Credentials
+func credentialsFromContext(ctx context.Context) (Input, error) {
+	c, ok := ctx.Value(credentialsKey).(Input)
+	if !ok {
+		return Input{}, errors.New("strategy: getting credentials key from context")
+	}
+	return c, nil
 }
 
-// IngestInput sets the input field of the underlying [Credentials].
-// [Credentials] is ready to call the rest of it's methods if this method returns no error.
-func (x *Credentials) IngestInput(ctx context.Context, input CredentialsInput) error {
-	_, span := tracer.Start(ctx, "IngestInput", trace.WithAttributes(input.TraceAttributes()...))
-	defer span.End()
-
-	p, err := password.FromString(input.Password)
-	if err != nil {
-		return fmt.Errorf("strategy: credentials, %w", err)
-	}
-	if err = validation.Email(input.Email); err != nil {
-		return fmt.Errorf("strategy: credentials, %w", err)
-	}
-
-	x.email = input.Email
-	x.password = p
-
-	return nil
+func NewContext(ctx context.Context, c Input) context.Context {
+	return context.WithValue(ctx, credentialsKey, c)
 }
 
 // Register will handles registration with the credentials strategy.
@@ -86,17 +65,34 @@ func (x *Credentials) Register(ctx context.Context) error {
 	ctx, span := tracer.Start(ctx, "Register")
 	defer span.End()
 
+	cred, err := credentialsFromContext(ctx)
+	if err != nil {
+		return err
+	}
+	span.SetAttributes(
+		attribute.String("email", cred.Email),
+		attribute.Int("password length", utf8.RuneCountInString((cred.Password))),
+	)
+
+	p, err := password.FromString(cred.Password)
+	if err != nil {
+		return fmt.Errorf("strategy: credentials, %w", err)
+	}
+	if err = validation.Email(cred.Email); err != nil {
+		return fmt.Errorf("strategy: credentials, %w", err)
+	}
+
 	if err := credentials.Insert(ctx, x.db, credentials.InsertParams{
 		ID:        uuid.New(),
-		Email:     x.email,
-		Password:  x.password,
+		Email:     cred.Email,
+		Password:  p,
 		CreatedAt: time.Now(),
 	}); err != nil {
 		return err
 	}
 
 	if err := email.Ingest(ctx, x.natsConn, email.Email{
-		To:      x.email,
+		To:      cred.Email,
 		From:    email.TestFrom,
 		Subject: email.TestSubject,
 		Body:    email.TestBody,
