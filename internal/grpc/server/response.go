69349d7b0925285caf1cce1f715db3dd89e3e432
Salam4nder <kamran.personal@icloud.com>
2024-09-02 01:04:31 +0200

    chore: lint

--- internal/grpc/server/response.go
+++ internal/grpc/server/response.go
@@ -22,23 +22,23 @@ func internalServerError(ctx context.Context, err error) error {
 	return status.Error(codes.Internal, "internal server error, please provide the traceID to support")
 }
 
-func invalidArgumentError(ctx context.Context, err error, msg string) error {
-	if err != nil {
-		span := trace.SpanFromContext(ctx)
-		span.SetStatus(otelCode.Error, err.Error())
-		span.RecordError(err)
-	}
-	return status.Error(codes.InvalidArgument, msg)
-}
+// func invalidArgumentError(ctx context.Context, err error, msg string) error {
+// 	if err != nil {
+// 		span := trace.SpanFromContext(ctx)
+// 		span.SetStatus(otelCode.Error, err.Error())
+// 		span.RecordError(err)
+// 	}
+// 	return status.Error(codes.InvalidArgument, msg)
+// }
 
-func alreadyExistsError(ctx context.Context, err error, msg string) error {
-	if err != nil {
-		span := trace.SpanFromContext(ctx)
-		span.SetStatus(otelCode.Error, err.Error())
-		span.RecordError(err)
-	}
-	return status.Error(codes.AlreadyExists, msg)
-}
+// func alreadyExistsError(ctx context.Context, err error, msg string) error {
+// 	if err != nil {
+// 		span := trace.SpanFromContext(ctx)
+// 		span.SetStatus(otelCode.Error, err.Error())
+// 		span.RecordError(err)
+// 	}
+// 	return status.Error(codes.AlreadyExists, msg)
+// }
 
 // func unauthenticatedError(err error, span trace.Span, msg string) error {
 // 	span.SetStatus(otelCode.Error, err.Error())
