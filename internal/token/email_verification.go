f05916a7defea9ec25918e04aacc80f924e7f622
Salam4nder <kamran.personal@icloud.com>
2024-09-08 22:42:44 +0200

    feat: email verification token

--- internal/token/email_verification.go
+++ internal/token/email_verification.go
@@ -1,15 +1,33 @@
 package token
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
-// NewEmailVerificationToken will return a token in the format of
-// sha256SumOf(email)/uuid.New
-func NewEmailVerificationToken(email string) string {
-	b := sha256.Sum256([]byte(email))
-	h := hex.EncodeToString(b[:])
-	return fmt.Sprintf("%s/%s", h, email)
+// NewEmailVerificationToken will return a token in the format of id/uuid.New.
+func NewEmailVerificationToken(id string) string {
+	return fmt.Sprintf("%s/%s", id, uuid.NewString())
+}
+
+func ParseVerificationToken(t string) (id uuid.UUID, token uuid.UUID, err error) {
+	s := strings.Split(t, "/")
+	if len(s) != 2 {
+		return uuid.Nil, uuid.Nil, errors.New("token: len of token is not 2")
+	}
+
+	id, err = uuid.Parse(s[0])
+	if err != nil {
+		return uuid.Nil, uuid.Nil, fmt.Errorf("token: parsing id, %w", err)
+	}
+
+	token, err = uuid.Parse(s[1])
+	if err != nil {
+		return uuid.Nil, uuid.Nil, fmt.Errorf("token: parsing token, %w", err)
+	}
+
+	return id, token, err
 }
