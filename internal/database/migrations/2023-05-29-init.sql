b2d8abc6b0913571aa335f02ce918940ed489f63
Salam4nder <kamran.personal@icloud.com>
2024-09-08 16:30:24 +0200

    feat: tokens table

--- internal/database/migrations/2023-05-29-init.sql
+++ internal/database/migrations/2023-05-29-init.sql
@@ -2,13 +2,18 @@ CREATE TABLE IF NOT EXISTS credentials (
     id uuid PRIMARY KEY,
     email varchar(255) NOT NULL UNIQUE,
     password_hash varchar(255) NOT NULL,
-    created_at timestamptz NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz DEFAULT NULL,
     verified_at timestamptz NULL
 );
 
 CREATE TABLE IF NOT EXISTS personal_numbers (
     id bigserial PRIMARY KEY,
-    created_at timestamptz NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tokens (
+    token TEXT UNIQUE PRIMARY KEY,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
