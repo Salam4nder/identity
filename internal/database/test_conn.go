1ebdca4d63a3c5cc9975db44e60fa40ee0641d6e
Salam4nder <kamran.personal@icloud.com>
2024-09-09 01:09:40 +0200

    refactor: new migrator

--- internal/database/test_conn.go
+++ internal/database/test_conn.go
@@ -6,15 +6,21 @@ package database
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
 	"time"
 
+	_ "embed"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/lib/pq"
 
 	"github.com/Salam4nder/identity/internal/config"
-	migrate "github.com/rubenv/sql-migrate"
+	"github.com/Salam4nder/identity/internal/database/migrations"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
 const connTimeout = 15 * time.Second
@@ -37,17 +43,22 @@ func SetupTestConn(tableName string) (*sql.DB, func()) {
 		os.Exit(1)
 	}
 
-	n, err := migrate.Exec(
-		db,
-		cfg.Driver(),
-		&migrate.FileMigrationSource{Dir: "../migrations"},
-		migrate.Up,
-	)
+	src, err := iofs.New(migrations.Files, ".")
 	if err != nil {
-		slog.Error("database: migrating", "err", err)
+		slog.Error("database: iofs", "err", err)
 		os.Exit(1)
 	}
-	slog.Info("database: applied migrations", "amount", n)
+	m, err := migrate.NewWithSourceInstance("iofs", src, cfg.Addr())
+	if err != nil {
+		slog.Error("database: creating source", "err", err)
+		os.Exit(1)
+	}
+	if err = m.Up(); err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			slog.Error("database: migrating", "err", err)
+			os.Exit(1)
+		}
+	}
 
 	return db, func() {
 		_, err := db.Exec(fmt.Sprintf("TRUNCATE %s CASCADE", tableName))
