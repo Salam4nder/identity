676ba17f9ecd09195444997b65fe3201d8020f57
Salam4nder <kamran.personal@icloud.com>
2024-09-22 16:17:13 +0200

    refactor: set identifiers in tokens

--- internal/token/paseto.go
+++ internal/token/paseto.go
@@ -1,13 +1,20 @@
 package token
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"aidanwoods.dev/go-paseto"
+	"github.com/Salam4nder/identity/proto/gen"
 )
 
 var _ Maker = (*PasetoMaker)(nil)
+
+const (
+	identifierKey = "token_identifier"
+	strategyKey   = "token_strategy"
+)
 
 // PasetoMaker makes PASETO tokens.
 type PasetoMaker struct {
@@ -40,20 +47,56 @@ func BootstrapPasetoMaker(
 	}, nil
 }
 
-func (x *PasetoMaker) MakeAccessToken() SafeString {
+func (x *PasetoMaker) MakeAccessToken(identifer any, strat gen.Strategy) (SafeString, error) {
 	token := paseto.NewToken()
+	switch strat {
+	case gen.Strategy_TypeCredentials:
+		s, ok := identifer.(string)
+		if !ok {
+			return "", fmt.Errorf("token: expected identifier to be string, got %T", identifer)
+		}
+		token.Set(strategyKey, gen.Strategy_TypeCredentials)
+		token.Set(identifierKey, s)
+	case gen.Strategy_TypePersonalNumber:
+		d, ok := identifer.(uint64)
+		if !ok {
+			return "", fmt.Errorf("token: expected identifier to be uint64, got %T", identifer)
+		}
+		token.Set(strategyKey, gen.Strategy_TypePersonalNumber)
+		token.Set(identifierKey, d)
+	default:
+		return "", errors.New("unsupported strategy")
+	}
 	token.SetIssuedAt(time.Now())
 	token.SetNotBefore(time.Now())
 	token.SetExpiration(time.Now().Add(x.accessDur))
-	return fromString(token.V4Encrypt(x.symmetricKey, nil))
+	return fromString(token.V4Encrypt(x.symmetricKey, nil)), nil
 }
 
-func (x *PasetoMaker) MakeRefreshToken() SafeString {
+func (x *PasetoMaker) MakeRefreshToken(identifer any, strat gen.Strategy) (SafeString, error) {
 	token := paseto.NewToken()
+	switch strat {
+	case gen.Strategy_TypeCredentials:
+		s, ok := identifer.(string)
+		if !ok {
+			return "", fmt.Errorf("token: expected identifier to be string, got %T", identifer)
+		}
+		token.Set(strategyKey, gen.Strategy_TypeCredentials)
+		token.Set(identifierKey, s)
+	case gen.Strategy_TypePersonalNumber:
+		d, ok := identifer.(uint64)
+		if !ok {
+			return "", fmt.Errorf("token: expected identifier to be uint64, got %T", identifer)
+		}
+		token.Set(strategyKey, gen.Strategy_TypePersonalNumber)
+		token.Set(identifierKey, d)
+	default:
+		return "", errors.New("unsupported strategy")
+	}
 	token.SetIssuedAt(time.Now())
 	token.SetNotBefore(time.Now())
 	token.SetExpiration(time.Now().Add(x.refreshDur))
-	return fromString(token.V4Encrypt(x.symmetricKey, nil))
+	return fromString(token.V4Encrypt(x.symmetricKey, nil)), nil
 }
 
 func (x *PasetoMaker) Verify(t SafeString) error {
