2614a64a9c8fd1e34f69aee7e45526a1e8b57f27
Salam4nder <kamran.personal@icloud.com>
2024-09-22 16:17:19 +0200

    refactor: token tests

--- internal/token/paseto_test.go
+++ internal/token/paseto_test.go
@@ -3,6 +3,9 @@ package token
 import (
 	"testing"
 	"time"
+
+	"github.com/Salam4nder/identity/pkg/random"
+	"github.com/Salam4nder/identity/proto/gen"
 )
 
 func bootstrap(t *testing.T) *PasetoMaker {
@@ -40,24 +43,76 @@ func bootstrap(t *testing.T) *PasetoMaker {
 
 func TestMakeAccessToken(t *testing.T) {
 	b := bootstrap(t)
-	s := b.MakeAccessToken()
+	s, err := b.MakeAccessToken(random.Email(), gen.Strategy_TypeCredentials)
+	if err != nil {
+		t.Error("expected no error")
+	}
 	if s == "" {
 		t.Error("token is empty")
 	}
+
+	t.Run("bad identifier", func(t *testing.T) {
+		b := bootstrap(t)
+		s, err := b.MakeAccessToken(100, gen.Strategy_TypeCredentials)
+		if err == nil {
+			t.Error("expected error")
+		}
+		if s != "" {
+			t.Error("expected empty string")
+		}
+	})
+	t.Run("bad strategy", func(t *testing.T) {
+		b := bootstrap(t)
+		s, err := b.MakeAccessToken(100, gen.Strategy_TypeNoStrategy)
+		if err == nil {
+			t.Error("expected error")
+		}
+		if s != "" {
+			t.Error("expected empty string")
+		}
+	})
 }
 
 func TestMakeRefreshToken(t *testing.T) {
-	b := bootstrap(t)
-	s := b.MakeRefreshToken()
-	if s == "" {
-		t.Error("token is empty")
-	}
+	t.Run("OK", func(t *testing.T) {
+		b := bootstrap(t)
+		s, err := b.MakeRefreshToken(random.Email(), gen.Strategy_TypeCredentials)
+		if err != nil {
+			t.Error("expected no error")
+		}
+		if s == "" {
+			t.Error("token is empty")
+		}
+	})
+	t.Run("bad identifier", func(t *testing.T) {
+		b := bootstrap(t)
+		s, err := b.MakeRefreshToken(100, gen.Strategy_TypeCredentials)
+		if err == nil {
+			t.Error("expected error")
+		}
+		if s != "" {
+			t.Error("expected empty string")
+		}
+	})
+	t.Run("bad strategy", func(t *testing.T) {
+		b := bootstrap(t)
+		s, err := b.MakeRefreshToken(100, gen.Strategy_TypeNoStrategy)
+		if err == nil {
+			t.Error("expected error")
+		}
+		if s != "" {
+			t.Error("expected empty string")
+		}
+	})
 }
 
 func TestVerify(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		b := bootstrap(t)
-		s := b.MakeAccessToken()
+		s, err := b.MakeAccessToken(random.Email(), gen.Strategy_TypeCredentials)
+		if err != nil {
+			t.Error("expected no error")
+		}
 		if err := b.Verify((s)); err != nil {
 			t.Errorf("expected no error, got %s", err.Error())
 		}
