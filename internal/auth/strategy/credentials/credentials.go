1520a13dc2746d0af6a965f62d39e956750fb871
Salam4nder <kamran.personal@icloud.com>
2024-09-23 22:09:08 +0200

    feat: authenticate w/ credentials

--- internal/auth/strategy/credentials/credentials.go
+++ internal/auth/strategy/credentials/credentials.go
@@ -20,6 +20,7 @@ import (
 	"github.com/nats-io/nats.go"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -28,7 +29,10 @@ var (
 	inputKey  ctxKey
 	outputKey ctxKey
 
+	ErrUserNotFound      = errors.New("credentials: user does not exist")
+	ErrUserNotVerified   = errors.New("credentials: user is not verified")
 	ErrTokenDoesNotExist = errors.New("credentials: token does not exist")
+	ErrIncorrectPassword = errors.New("credentials: incorrect password")
 )
 
 type (
@@ -62,7 +66,7 @@ func NewContext(ctx context.Context, c *Input) context.Context {
 func FromContext(ctx context.Context) (*Output, error) {
 	c, ok := ctx.Value(outputKey).(*Output)
 	if !ok {
-		return nil, errors.New("strategy: getting credentials output from context")
+		return nil, errors.New("credentials: getting credentials output from context")
 	}
 	return c, nil
 }
@@ -85,10 +89,10 @@ func (x *Strategy) Register(ctx context.Context) (context.Context, error) {
 
 	p, err := password.FromString(cred.Password)
 	if err != nil {
-		return ctx, fmt.Errorf("strategy: credentials, %w", err)
+		return ctx, fmt.Errorf("credentials: creating password from string, %w", err)
 	}
 	if err = validation.Email(cred.Email); err != nil {
-		return ctx, fmt.Errorf("strategy: credentials, %w", err)
+		return ctx, fmt.Errorf("credentials: validating email, %w", err)
 	}
 
 	tx, err := x.db.BeginTx(ctx, nil)
@@ -133,11 +137,50 @@ func (x *Strategy) Register(ctx context.Context) (context.Context, error) {
 	return newContext(ctx, &Output{Email: cred.Email}), nil
 }
 
-func (x *Strategy) Authenticate(_ context.Context) error {
+// Authenticate will authenticate a user.
+// Possible errors are [ErrIncorrectPassword], [ErrUserNotFound], [ErrUserNotVerified] and a wrapped error
+// indicating an internal error.
+func (x *Strategy) Authenticate(ctx context.Context) error {
+	ctx, span := tracer.Start(ctx, "Authenticate")
+	defer span.End()
+
+	cred, err := fromContext(ctx)
+	if err != nil {
+		return err
+	}
+	span.SetAttributes(
+		attribute.String("email", cred.Email),
+		attribute.Int("password length", utf8.RuneCountInString((cred.Password))),
+	)
+
+	p, err := password.FromString(cred.Password)
+	if err != nil {
+		return ErrIncorrectPassword
+	}
+
+	e, err := credentials.ReadByEmail(ctx, x.db, cred.Email)
+	if err != nil {
+		if errors.As(err, &database.NotFoundError{}) {
+			return ErrUserNotFound
+		}
+		return fmt.Errorf("credentials: reading by email, %w", err)
+	}
+
+	if e.VerifiedAt == nil || e.VerifiedAt.IsZero() {
+		return ErrUserNotVerified
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(e.PasswordHash), []byte(p)); err != nil {
+		return fmt.Errorf("credentials: comparing password hash, %w", err)
+	}
+
 	return nil
 }
 
-func (x *Strategy) Verify(ctx context.Context, tokenInput string) error {
+func (x *Strategy) VerifyEmail(ctx context.Context, tokenInput string) error {
+	ctx, span := tracer.Start(ctx, "VerifyEmail")
+	defer span.End()
+
 	t, err := tokendb.Get(ctx, x.db, tokenInput)
 	if err != nil {
 		if errors.As(err, &database.NotFoundError{}) {
@@ -187,7 +230,7 @@ func (x *Strategy) Verify(ctx context.Context, tokenInput string) error {
 func fromContext(ctx context.Context) (*Input, error) {
 	c, ok := ctx.Value(inputKey).(*Input)
 	if !ok {
-		return nil, errors.New("strategy: getting credentials input from context")
+		return nil, errors.New("credentials: getting credentials input from context")
 	}
 	return c, nil
 }
