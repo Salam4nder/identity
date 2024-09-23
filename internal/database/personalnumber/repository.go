da91ffaf6f446882175ade29d7e6c6bb897140e4
Salam4nder <kamran.personal@icloud.com>
2024-09-23 22:29:33 +0200

    feat: get personal number

--- internal/database/personalnumber/repository.go
+++ internal/database/personalnumber/repository.go
@@ -3,6 +3,7 @@ package personalnumber
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/Salam4nder/identity/internal/database"
 	"go.opentelemetry.io/otel"
@@ -37,6 +38,31 @@ func Insert(ctx context.Context, db *sql.DB, id uint64) error {
 	}
 
 	return nil
+}
+
+func Get(ctx context.Context, db *sql.DB, id uint64) (uint64, error) {
+	ctx, span := tracer.Start(ctx, "Delete")
+	defer span.End()
+	span.SetAttributes(attribute.Int64("id", int64(id)))
+
+	if id == 0 {
+		return 0, database.NewInputError(ctx, errors.New("id is empty"), "id", id)
+	}
+
+	query := `
+    SELECT id personal_numbers
+    WHERE id = $1
+    `
+	span.SetAttributes(attribute.String("query", query))
+
+	var res uint64
+	if err := db.QueryRowContext(ctx, query, id).Scan(&res); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, database.NewNotFoundError(ctx, err, "personal_number", id)
+		}
+		return 0, database.NewOperationFailedError(ctx, err)
+	}
+	return res, nil
 }
 
 func Delete(ctx context.Context, db *sql.DB, id uint64) error {
